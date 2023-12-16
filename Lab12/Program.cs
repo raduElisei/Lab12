346526a9f7b5c78660a82bcf0cf8862f99983a74
Radu <45122923+raduElisei@users.noreply.github.com>
2023-12-16 21:55:37 +0200

    Add project files.

---
+++ Lab12/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab12;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string exampleString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut" +
+            " labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip" +
+            " ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+            " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+        var transformer = TextTransformer.FromString(exampleString);
+        transformer
+            .ReplaceSubstring("Lorem", "mamaliga")
+            .DeleteSubstring("laborum")
+            .ConvertToUppercase();
+
+        transformer.PrintString();
+    }
+}
---
+++ Lab12/TextTransform.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace Lab12;
+
+internal class TextTransformer
+{
+    private StringBuilder textBuilder;
+
+    private TextTransformer(string initialText)
+    {
+        textBuilder = new StringBuilder(initialText);
+    }
+
+    public static TextTransformer FromString(string initialText)
+    {
+        return new TextTransformer(initialText);
+    }
+
+    public TextTransformer ReplaceSubstring(string oldSubstring, string newSubstring)
+    {
+        textBuilder.Replace(oldSubstring, newSubstring);
+        return this;
+    }
+
+    public TextTransformer ConvertToUppercase()
+    {
+        textBuilder = new StringBuilder(textBuilder.ToString().ToUpper());
+        return this;
+    }
+
+    public TextTransformer ConvertToLowercase()
+    {
+        textBuilder = new StringBuilder(textBuilder.ToString().ToLower());
+        return this;
+    }
+
+    public TextTransformer DeleteSubstring(string substring)
+    {
+        textBuilder.Replace(substring, string.Empty);
+        return this;
+    }
+
+    public TextTransformer InsertSubstring(int position, string substring)
+    {
+        if (position >= 0 && position <= textBuilder.Length)
+        {
+            textBuilder.Insert(position, substring);
+        }
+        else
+        {
+            Console.WriteLine($"Position {position} is invalid.");
+        }
+
+        return this;
+    }
+
+    public void PrintString()
+    {
+        Console.WriteLine(textBuilder.ToString());
+    }
+}
